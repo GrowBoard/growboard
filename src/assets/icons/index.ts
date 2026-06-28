7efe8bf1b22023a4c906178bf8f07b1c28957d8e
Amit Raikwar <raikwar.amit.git@gmail.com>
2026-06-28 20:12:22 +0530

    chore(assets): remove unused cred_icon and social_icon folders

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,7 +1,5 @@
 export * from './alert_icon';
-export * from './cred_icon';
 export * from './profile_icon';
 export * from './project_icon';
 export * from './sidebar_icons';
-export * from './social_icon';
 export * from './GrowboardIcon';
